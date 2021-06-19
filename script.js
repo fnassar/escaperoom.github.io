3c63525381945c537ff532378b7812403b43eab1
fnassar <fan6236@nyu.edu>
2021-06-19 23:39:07 +0400

    added dummy images and sound

--- script.js
+++ script.js
@@ -37,3 +37,29 @@ function update(e){
 
 document.addEventListener('mousemove',update)
 document.addEventListener('touchmove',update)
+
+function playaudio(obj, aud){
+	obj.onmouseover = e => {
+		obj.style.backgroundColor = "red";
+		aud.play();
+	}
+	obj.onmouseleave = e => {
+		obj.style.removeProperty("background-color");
+		aud.pause();
+	}
+}
+
+var objArr = Array(7);
+var objnames = ["obj1","obj2","obj3","obj4","obj5","obj6","obj7"];
+
+var audioArr = Array(7);
+var audionames = ["audio1","audio2","audio3","audio4","audio5","audio6","audio7"];
+
+for(let i=0; i<7; i++){
+	objArr[i] = document.getElementById(objnames[i]);
+	audioArr[i] = document.getElementById(audionames[i]);
+}
+
+for(let j=0; j<7; j++){
+	playaudio(objArr[j],audioArr[j]);
+}
