12b07c9d2073e26b136eabdaa531771f52c20190
karishev <71120362+karishev@users.noreply.github.com>
2021-06-20 23:50:19 +0600

    uploading

---
+++ Cheena's code/script.js
@@ -0,0 +1,86 @@
+let nameoftheuser;
+
+document.addEventListener('DOMContentLoaded', () => {
+	let title = document.querySelector('.title');
+	title.style.cursor = "default";
+	let useranswer = document.querySelector('.user');
+	title.addEventListener("keyup", (event) => {
+		if (event.key === "Enter") {
+			nameoftheuser = useranswer.value;
+			title.style.cursor = "none";
+			title.classList.add('fadeout');
+			let headphones = document.querySelector('.headphones');
+			headphones.style.opacity = "0";
+			
+			useranswer.disabled = true;
+			enableScroll();
+			setTimeout(() => {
+				title.remove();
+				
+			}, 4000);
+			
+			setTimeout(() => {
+				headphones.remove();
+				document.querySelector('.start').classList.add('fadein');
+			}, 4000);
+		}
+	})
+})
+
+
+function enableScroll() {
+	window.onscroll = function() {};
+}
+
+
+let gamescreen = document.querySelector(".start");
+let beforegame = window.getComputedStyle(gamescreen, "::before");
+
+// $("body").mousemove(function(e) {
+// 	console.log(e.pageX);
+// 	console.log(e.pageY);
+// })
+
+function update(e){
+
+	var x = e.clientX || e.touches[0].clientX;
+	var y = e.clientY || e.touches[0].clientY;
+
+	var style = document.querySelector('.start').style;
+
+	if (numofclicks > 0) {
+		style.setProperty('--cursorX', x + 'px');
+		style.setProperty('--cursorY', y + 'px');
+	}
+
+}
+let flash = document.getElementById('flash');
+let check = 0;
+let numofclicks = 0;
+function blink() {
+	let style = document.querySelector('.shelf').style;
+	numofclicks = 1;
+	if (check == 0) {
+		style.opacity = "1";
+		check = 1;
+		flash.load();
+		flash.play();
+	}
+	else { 
+		style.opacity = "5%";
+		check = 0;
+		flash.load();
+		flash.play();
+	}
+}
+
+
+
+// function to_start() {
+// 	numofclicks++;
+
+// }
+
+
+document.querySelector('.start').addEventListener('mousemove',update)
+document.querySelector('.start').addEventListener('touchmove',update)
